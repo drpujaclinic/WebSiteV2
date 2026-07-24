4809afcb38e3d719c3cbfbfe2a304fda805f2e12
Dr Mani Shankar <dr.mani.shankar@gmail.com>
2026-07-24 07:59:54 +0530

    Add staff login in api

---
+++ api/staff/login.php
@@ -0,0 +1,3 @@
+<?php
+require dirname($_SERVER['DOCUMENT_ROOT']) . '/backend/api/staff/login.php';
+// api/staff/login.php
---
+++ api/staff/verify-totp.php
@@ -0,0 +1,3 @@
+<?php
+require dirname($_SERVER['DOCUMENT_ROOT']) . '/backend/api/staff/verify-totp.php';
+// api/staff/verify-totp.php
